2224a9c4db5a0805fc3828628443fc1249193ca6
Cj Adonis <cjadonis20@gmail.com>
2023-04-08 15:14:32 +0800

    setup initial google sign in firebase

--- app/src/main/java/com/adonis/base/arch/ui/login/Dashboard.kt
+++ app/src/main/java/com/adonis/base/arch/ui/login/Dashboard.kt
@@ -1,9 +1,12 @@
 package com.adonis.base.arch.ui.login
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import androidx.core.view.isVisible
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -14,9 +17,12 @@ import com.adonis.base.arch.ui.viewmodels.SampleViewModel
 import com.adonis.base.databinding.FragmentDashboardBinding
 import com.adonis.base.extensions.showShortToast
 import com.adonis.base.util.UserPreferences
+import com.google.android.gms.auth.api.identity.*
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -25,38 +31,73 @@ class Dashboard : BaseFragment<FragmentDashboardBinding>() {
     @Inject
     lateinit var userPreferences: UserPreferences
 
-    private val viewModel : SampleViewModel by activityViewModels()
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
+
+    private val viewModel: SampleViewModel by activityViewModels()
+
+    private val oneTapClient by lazy { Identity.getSignInClient(requireActivity()) }
+
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+
+                oneTapClient.runCatching {
+                    getSignInCredentialFromIntent(it.data)
+                }.onSuccess { signInCredential ->
+                    val token = signInCredential.googleIdToken
+
+                    if (token != null) {
+                        Timber.e(token)
+                    }
+
+                }.onFailure {
+                    requireActivity().showShortToast("No google account signed-in detected.")
+                }
+            }
+        }
 
     override val bindingInflater: (LayoutInflater) -> FragmentDashboardBinding
         get() = FragmentDashboardBinding::inflate
 
     override fun initViews(view: View, savedInstanceState: Bundle?) {
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        actionBar?.hide()
 
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.CREATED){
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
                 userPreferences.getIsUserNew.collectLatest { isUserNew ->
-                    if(isUserNew){
+                    if (isUserNew) {
                         OnboardingActivity.startActivity(requireContext())
                     }
                 }
             }
         }
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.jokeState.collectLatest {
-                    binding.loading.isVisible = it.isLoading
-                    binding.textView.text = it.data?.joke
+        binding.googleLoginButton.setOnClickListener {
 
-                    it.exception?.let { message ->
-                        requireActivity().showShortToast(message)
-                    }
-                }
-            }
         }
+    }
 
-        binding.getJokeButton.setOnClickListener {
-            viewModel.getJoke()
+    private fun initGoogleSignInRequest() {
+        val signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId("457234146619-f40fff56pc5ejd7fqpjibidm0sjsgu0g.apps.googleusercontent.com")
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            ).build()
+
+        oneTapClient.beginSignIn(signInRequest).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val intentSender = task.result.pendingIntent.intentSender
+                val intentSenderRequest = IntentSenderRequest.Builder(intentSender).build()
+                signInLauncher.launch(intentSenderRequest)
+            }else{
+                Timber.e(task.exception)
+                requireActivity().showShortToast("Unknown error occurred.")
+            }
         }
     }
 }
--- app/src/main/java/com/adonis/base/di/module/FragmentModule.kt
+++ app/src/main/java/com/adonis/base/di/module/FragmentModule.kt
@@ -1,8 +1,13 @@
 package com.adonis.base.di.module
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.scopes.FragmentScoped
 
 @InstallIn(FragmentComponent::class)
 @Module
@@ -13,5 +18,11 @@ object FragmentModule {
     //    fun provideMyService(): MyService {
     //        return MyService()
     //    }
+
+    @FragmentScoped
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return Firebase.auth
+    }
 
 }
